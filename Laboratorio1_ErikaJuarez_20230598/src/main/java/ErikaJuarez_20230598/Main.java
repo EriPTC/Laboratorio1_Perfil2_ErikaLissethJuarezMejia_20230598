fb3c26764d37979889a8f845910af218c24d3bed
ERIKA <20230598@ricaldone.edu.sv>
2025-06-06 10:30:28 -0600

    menu

--- Laboratorio1_ErikaJuarez_20230598/src/main/java/ErikaJuarez_20230598/Main.java
+++ Laboratorio1_ErikaJuarez_20230598/src/main/java/ErikaJuarez_20230598/Main.java
@@ -1,7 +1,37 @@
 package ErikaJuarez_20230598;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido, que desea hacer?");
+        System.out.println("1-Ingresar");
+        System.out.println("2-Actualizar");
+        System.out.println("3-Eliminar");
+        System.out.println("4-Salir");
+        System.out.println("Ingrese una opcion ");
+        int menu = sc.nextInt();
+
+        do {
+            switch (menu){
+                case 1 :
+                    System.out.println("H");
+                break;
+                case 2 :
+                    System.out.println( "o");
+                    break;
+                case 3 :
+                    System.out.println("l");
+                    break;
+                case 4 :
+                    System.out.println("Hasta pronto");
+                    break;
+            }
+
+        } while (menu !=4);
+
+
+
     }
 }
