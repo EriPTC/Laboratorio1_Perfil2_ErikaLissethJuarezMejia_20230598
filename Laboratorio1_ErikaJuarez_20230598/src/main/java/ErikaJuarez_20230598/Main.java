4b52a6af4ecbeefc2da4120517ecbac93181226e
ERIKA <20230598@ricaldone.edu.sv>
2025-06-06 11:09:24 -0600

    ingresar a medias

--- Laboratorio1_ErikaJuarez_20230598/src/main/java/ErikaJuarez_20230598/Main.java
+++ Laboratorio1_ErikaJuarez_20230598/src/main/java/ErikaJuarez_20230598/Main.java
@@ -1,36 +1,50 @@
 package ErikaJuarez_20230598;
 
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
+
     public static void main(String[] args) {
+        TreeMap<String, Double> tm= new TreeMap<>();
+        productos objtreemap = new productos();
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido, que desea hacer?");
         System.out.println("1-Ingresar");
         System.out.println("2-Actualizar");
         System.out.println("3-Eliminar");
-        System.out.println("4-Salir");
+        System.out.println("4-Ver productos");
+        System.out.println("5-Salir");
         System.out.println("Ingrese una opcion ");
         int menu = sc.nextInt();
 
-        do {
-            switch (menu){
-                case 1 :
-                    System.out.println("H");
+        switch (menu){
+            case 1 :
+                System.out.println("Ingresa el producto");
+                String clave = sc.next();
+                System.out.println("ingrese el precio");
+                double valor = sc.nextDouble();
+                tm.put(clave,valor);
+                System.out.println("producto ingresado: " +clave +", " +valor);
                 break;
-                case 2 :
-                    System.out.println( "o");
-                    break;
-                case 3 :
-                    System.out.println("l");
-                    break;
-                case 4 :
-                    System.out.println("Hasta pronto");
-                    break;
-            }
+            case 2 :
+                System.out.println();
+                break;
+            case 3 :
+                System.out.println("l");
+                break;
+            case 4 :
+                System.out.println(tm);
+                break;
+            case 5 :
+                System.out.println("Hasta pronto");
+                break;
+        }
 
-        } while (menu !=4);
+        do {
 
+
+        } while (menu !=5);
 
 
     }
--- Laboratorio1_ErikaJuarez_20230598/src/main/java/ErikaJuarez_20230598/productos.java
+++ Laboratorio1_ErikaJuarez_20230598/src/main/java/ErikaJuarez_20230598/productos.java
@@ -1,4 +1,20 @@
 package ErikaJuarez_20230598;
 
+import java.util.TreeMap;
+
+
 public class productos {
+
+    TreeMap<String, Double> tm= new TreeMap<>();
+
+    public void agregarProductos () {
+        this.tm = tm;
+        tm.put("cable C", 2.50);
+        tm.put("Luces led", 12.50);
+        tm.put("Teclado", 25.00);
+    }
+
+    public void mostrarProductos (){
+        System.out.println(tm);
+    }
 }
